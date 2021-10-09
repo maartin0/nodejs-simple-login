632dcc52f21e7486668f2f9d978a3cbd75b63465
leysper032 <martinmmps@gmail.com>
2021-10-09 18:48:48 +0000

    Login and registration functions finished, session functionality 80% done

--- server/auth.js
+++ server/auth.js
@@ -52,7 +52,18 @@ function hash_and_salt_string(value, salt) {
     return sha_256(salted_psk);
 }
 
-// External Functions
+// Miscelaneous Methods
+function get_unix_time() {
+    return (+ new Date());
+}
+
+function is_integer(value) {
+  return !isNaN(value) && 
+         parseInt(Number(value)) == value && 
+         !isNaN(parseInt(value, 10));
+}
+
+// Authentication Functions
 function register(user, psk) {
     // Function that registers a new user on the system with the new username and password.
     // The password should already be hashed with sha 256 by the client.
@@ -66,11 +77,12 @@ function register(user, psk) {
     const new_psk = hash_and_salt_string(psk, new_salt);
     const new_id = uuid.v4();
 
-    credentials[user] = {};
-    
-    credentials[user].psk = new_psk;
-    credentials[user].id = new_id;
-    credentials[user].salt = new_salt;
+    credentials.user_map[user] = new_id;
+
+    credentials.auth_map[new_id] = {
+        "psk": new_psk,
+        "salt": new_salt
+    };
 
     save_credentials();
     return true;
@@ -85,15 +97,33 @@ function login(user, psk) {
     
     if (!user_exists(user)) return false;
 
-    const stored_psk = credentials[user].psk;
-    const salt = credentials[user].salt;
+    const user_id = credentials.user_map[user];
+
+    var auth_user = credentials.aut_map[user_id];
+
+    const stored_psk = auth_user.psk;
+    const salt = auth_user.salt;
 
     const calculated_psk = hash_and_salt_string(psk, salt);
 
     if (stored_psk != calculated_psk) return false;
     
-    get_session(user);
+    if (get_session(user) == null) return false;
     return true;
+}
+
+function create_session(user_id) {
+    const session_id = uuid.v4();
+
+    credentials.auth_map[user_id].session = session_id;
+
+    credentials.session_map[session_id] = {
+        "expiration": (get_unix_time() + (60 * 60 * 1000)).toString(),
+        "user_id": user_id
+    }
+    
+    save_credentials();
+    return session_id;
 }
 
 function get_session(user) {
@@ -101,16 +131,35 @@ function get_session(user) {
     // if none is valid, it will generate a new one, 
     // if the user doesn't exist it'll return null;
     
+    if (!user_exists(user)) return null;
+
+    const user_id = credentials.user_map[user];
+
+    // Check user has session id
+    if ("session" in credentials.auth_map[user_id]) {
+        const session_id = credentials.auth_map[user_id].session;
+        // Check if session has linked expiration value
+        if ("expiration" in credentials.session_map[session_id]) {
+            const expiration = credentials.session_map[session_id].exipration;
+            // Confirm expiration is valid integer, and hasn't expired yet
+            if (is_integer(expiration) && parseInt(user_credentials.expiration) >= get_unix_time()) {
+                return session_id;
+            }
+        }
+    }
+
+    return create_session(user_id);
 }
 
-function session(key) {
+function session(session_id) {
     // Function that checks if a session key is valid, returns the user id
 }
 
 function user_exists(user) {
-    return user in credentials;
+    return user in credentials.user_map;
 }
 
 // const result = register("yay", "this is clearly hashed lol");
-const result = login("yay", "this is clearly hashed lol");
+// const result = login("yay", "this is clearly hashed lol");
+const result = get_session("yay");
 console.log(result);

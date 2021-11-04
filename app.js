a257481c9e1621ca5ce7eb08e8df473182b687dc
leysper032 <martinmmps@gmail.com>
2021-11-04 14:25:45 +0000

    All auth backend has been deleted. Files restructured. Auth frontend complete.

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = new express();
+const port = 80;
+
+// ----------------------------------------------------
+
+app.use(express.static(__dirname + "/client/static/"));
+app.set('views', __dirname + "/client/templates/");
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+const csrf = require('csurf');
+app.use(csrf({ cookie: true }));
+
+// ----------------------------------------------------
+
+const auth = require("./auth/router.js");
+app.use("/", auth);
+
+// ----------------------------------------------------
+
+app.listen(port, () => {
+  console.log(`Server Listening at http://localhost:${port}`);
+});

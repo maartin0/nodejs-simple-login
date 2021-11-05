288d05c33b7fcc74e4a0dd46a0d03a23f6f94ad8
leysper032 <martinmmps@gmail.com>
2021-11-05 08:03:29 +0000

    file.js created to make handling with JSON data in files simpler & neater

---
+++ files/file.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const fp = fs.promises;
+
+var path;
+var file;
+var content;
+
+var default_json = {};
+
+async function load(input_path, default_content) {
+    path = input_path;
+    default_json = default_content;
+
+    await reload();
+}
+
+async function create_default() {
+    await fp.writeFile(path, JSON.stringify(default_json));
+}
+
+async function reload() {
+    try {
+        content = await fp.readFile(path);
+    } catch {
+        await create_default();
+        reload();
+    }
+}
+
+async function save() {
+    await fp.writeFile(path, JSON.stringify(content));
+}

7af0c3d28ad3405cf3caf113f0d89b69fa06d28b
leysper032 <martinmmps@gmail.com>
2021-11-06 16:58:16 +0000

    More auth.js progress (4)

--- auth/auth.js
+++ auth/auth.js
@@ -3,6 +3,9 @@ const uuid = require("uuid");
 const bcrypt = require("bcrypt-promise");
 const rounds = 10;
 
+var get_timestamp = () => + new Date();
+const session_length = 60 * 60;
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -18,16 +21,19 @@ async function attempt(target, delay, limit, args) {
     return false;
 }
 
+async function get_user_file(id) {
+    return "users/" + id + ".json";
+}
+
 async function get_user_id(username) {
-    const register_json = await files.read("registered_users.json");
+    const register_json = await files.read("users.json");
     return register_json[username];
 }
 
 async function set_user_id(username, user_id) {
-    const register_file = await files.init("registered_users.json");
+    const register_file = await files.init("users.json");
     if (register_file === null) return false;
     register_file.json[username] = user_id;
-    console.log(register_file);
     await files.save(register_file);
     return true;
 }
@@ -36,8 +42,7 @@ async function register(username, password) {
     if (await get_user_id(username) != null) return false;
 
     const user_id = uuid.v4();
-    const user_path = user_id + ".json";
-    const user_data = await files.init(user_path);
+    const user_data = await files.init(await get_user_file(user_id));
 
     if (user_data === null) return false;
     if (user_data.content !== "{}") return false;
@@ -51,17 +56,31 @@ async function register(username, password) {
     return await attempt(set_user_id, 1000, 5, [username, user_id]);
 }
 
+async function create_session(user_id) {
+    return session_id;
+}
+
+async function check_session(session_id) {
+    return is_valid;
+}
+
+async function delete_session(session_id) {
+    return is_valid;
+}
+
 async function login(username, password) {
-    const auth_file = await files.init("auth.json");
-    const hash = auth_file.json[username];
-    const result = await bcrypt.compare(password, hash);
-    console.log(result);
+    // Check if user exists
+    const user_id = await get_user_id(username);
+    if (user_id == null) return false;
 
-    return result; 
+    // Check if password is correct
+    const user_path = await get_user_file(user_id);
+    const user_data = await files.read(user_path);
+    const hash = user_data.hash;
+    if (!bcrypt.compare(password, hash)) return false;
+
+    const old_session = user_data.session;
+
 }
 
-async function user_exists(user) {
-
-}
-
-register("hello", "world").then((result) => console.log(result));
+login("hello", "world").then((result) => console.log(result));

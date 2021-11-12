68455a49a7d1db2280b5110ae1f24779807fd712
leysper032 <martinmmps@gmail.com>
2021-11-12 20:17:21 +0000

    More auth.js progress (15)

--- auth/auth.js
+++ auth/auth.js
@@ -1,4 +1,4 @@
-const files = require("../files/file.js");
+const files = require("../file.js");
 const uuid = require("uuid");
 
 const bcrypt = require("bcrypt-promise");
@@ -6,6 +6,12 @@ const rounds = 10;
 
 // Session length in ms (Default: 1 hour)
 const session_length = 60 * 60 * 1000;
+
+async function init() {
+    // Open and close users.json file to verify that file exists to allow reading of file later, without writing.
+    var users_file = await files.init("users.json");
+    await files.close(users_file);
+}
 
 /* TIME FUNCTIONS */
 async function now() {
@@ -18,10 +24,7 @@ function sleep(ms) {
 
 async function has_expired(check) {
     const cur = await now();
-    console.log("Current epoch: " + cur);
-    console.log("Supplied: " + check);
-    const result = check > cur;
-    console.log("Is invalid: " + result);
+    const result = check < cur;
     return result;
 }
 
@@ -29,8 +32,7 @@ async function get_expiry() {
     return (await now()) + session_length;
 }
 
-/* Attempt function used for registering. Required in case of concurrent modification of the user - id map file. */
-
+// Attempt function used for registering. Required in case of concurrent modification of the user-id map file.
 async function attempt(target, delay, limit, args) {
     var attempt = 0;
     while (attempt < limit) {
@@ -41,13 +43,6 @@ async function attempt(target, delay, limit, args) {
         await sleep(delay);
     }
     return false;
-}
-
-/* MISC FUNCTIONS */
-
-async function init() {
-    var users_file = await files.init("users.json");
-    await files.close(users_file);
 }
 
 /* PATH & FILE FUNCTIONS */
@@ -70,8 +65,11 @@ async function init_user_file(id) {
 /* USER ID FUNCTIONS */
 
 async function get_user_id(username) {
+    if (username == null) return false;
     const register_json = await files.read("users.json");
-    return register_json[username];
+    try {
+        return register_json[username];
+    } catch { return null; }
 }
 
 async function set_user_id(username, user_id) {
@@ -82,7 +80,16 @@ async function set_user_id(username, user_id) {
     return true;
 }
 
+async function get_user_name(user_id) {
+    if (user_id == null) return null;
+    const user_data_path = await get_user_file_path(user_id);
+    const user_data = await files.read(user_data_path);
+    if (user_data == null) return null;
+
+}
+
 /* SESSION FUNCTIONS */
+
 async function create_session(user_id) {
     if (user_id == null) return false;
 
@@ -122,73 +129,53 @@ async function create_session(user_id) {
 }
 
 async function get_session(user_id) {
-    console.count("Get");
     if (user_id == null) return null;
 
     const user_file_path = await get_user_file_path(user_id);
     if (!await files.exists(user_file_path)) return null;
-    console.count("Get");
 
     var user_file = await files.read(user_file_path);
     if (user_file === null) return null;
-    console.count("Get");
 
     const session_id = user_file.session;
     const valid = await check_session(session_id);
-    console.count("Get");
-    console.log(session_id);
 
     if (valid) return session_id;
-    console.count("Get");
 
     const success = await create_session(user_id);
     if (!success) return null;
-    console.count("Get");
 
     user_file = await files.read(user_file_path);
-    console.count("Get");
-    console.log(user_file);
 
     const new_session_id = user_file.session;
     const new_valid = await check_session(new_session_id);
-    console.count("Get");
-    console.log(new_session_id);
-
     if (valid) return new_session_id;
-    console.count("Get");
     return null;
 }
 
 async function check_session(session_id) {
-    console.count("Check");
     if (session_id == null) return false;
 
     const session_file_path = await get_session_file_path(session_id);
     if (!files.exists(session_file_path)) return false;
-    console.count("Check");
 
     var session_file = await files.read(session_file_path);
     if (session_file === null) return false;
-    console.count("Check");
 
     const user_id = session_file.user_id;
     if (user_id == null) return false;
-    console.count("Check");
     
     const user_file_path = await get_user_file_path(user_id);
     if (!await files.exists(user_file_path)) return null;
-    console.count("Check");
     var user_file = await files.read(user_file_path);
 
     const expiry = user_file.expiry;
     if (expiry == null) return false;
-    console.count("Check");
     
     if (await has_expired(expiry)) {
-        // await delete_session(session_id);
+        await delete_session(session_id);
         return false;
     }
-    console.count("Check");
 
     return true;
 }
@@ -264,48 +251,29 @@ async function login(username, password) {
     return true;
 }
 
-/* DEVELOPMENT FUNCTIONS */
+/* ACCOUNT FUNCTIONS */
 
-async function dregister() {
-    const register_result = await register("hello", "world");
-    console.log("Register " + register_result);
+module.exports = {
+    "entry": {
+        "login": login,
+        "register": register
+    },
+    "session": {
+        "get": get_session,
+        "delete": delete_session,
+        "verify": check_session
+    },
+    "account": {
+        "delete": undefined,
+        "modify": {
+            "username": undefined
+            "password": undefined
+        }
+    },
+    "get": {
+        "user": {
+            "id": get_user_id,
+            "name": undefined
+        }
+    }
 }
-
-async function dlogin() {
-    const login_result = await login("hello", "world");
-    console.log("Login " + login_result);
-}
-
-async function duser() {
-    const user_id = await get_user_id("hello");
-    console.log("User ID: " + user_id);
-    return user_id;
-}
-
-async function dcheck() {
-    const session_id = "0936b522-8d63-4e30-80d0-ae4c0dc39c8c";
-    const session_validity = await check_session(session_id);
-    console.log("Session Valid: " + session_validity);
-}
-
-async function dcreate() {
-    const user_id = "b8592813-16e0-4b17-9967-61c57ec602e3";
-    const session_success = await create_session(user_id);
-    console.log("Create Success: " + session_success);
-}
-
-async function dget() {
-    const session_id = await get_session(user_id);
-    console.log("Session ID: " + session_id);
-}
-
-async function run() {
-    await init();   
-
-    await dcheck();
-}
-
-run();
-
-
-// UNCOMMENT 118 DELETE TEMPORARY ONLY
